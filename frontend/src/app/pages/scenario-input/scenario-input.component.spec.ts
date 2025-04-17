d2f5a6a24c58b0d7a2f589248fd21121556493f7
Suhas Puttoju <suhasp@unc.edu>
2025-04-17 02:47:52 +0000

    Changed to classic angular setup

--- frontend/src/app/pages/scenario-input/scenario-input.component.spec.ts
+++ frontend/src/app/pages/scenario-input/scenario-input.component.spec.ts
@@ -8,7 +8,7 @@ describe('ScenarioInputComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ScenarioInputComponent]
+      declarations: [ScenarioInputComponent]
     })
     .compileComponents();
 

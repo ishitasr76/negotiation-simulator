c00988a32979aab9ec36a04b46552e2d0c9ab929
Suhas Puttoju <suhasp@unc.edu>
2025-04-17 02:39:12 +0000

    Fixed SSR - Changed it

---
+++ frontend/src/app/pages/scenario-input/scenario-input.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scenario-input',
+  imports: [],
+  templateUrl: './scenario-input.component.html',
+  styleUrl: './scenario-input.component.css'
+})
+export class ScenarioInputComponent {
+
+}

57a20909726acd3df591f7cb245150b5685e008c
Suhas Puttoju <suhasp@unc.edu>
2025-04-17 20:54:02 +0000

    can't get this to work

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ScenarioInputComponent } from './pages/scenario-input/scenario-input.component';
+import { ResultsComponent } from './pages/results/results.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'input', component: ScenarioInputComponent },
+  { path: 'results', component: ResultsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

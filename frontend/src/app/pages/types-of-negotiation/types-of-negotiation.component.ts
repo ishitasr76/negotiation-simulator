85f561d36f9162748f6869225f8c7dc17afb409a
ishitasr76 <144747472+ishitasr76@users.noreply.github.com>
2025-04-20 17:27:09 -0400

    added navbar (#4)

---
+++ frontend/src/app/pages/types-of-negotiation/types-of-negotiation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-types-of-negotiation',
+  standalone: false,
+  templateUrl: './types-of-negotiation.component.html',
+  styleUrl: './types-of-negotiation.component.css'
+})
+export class TypesOfNegotiationComponent {
+
+}
